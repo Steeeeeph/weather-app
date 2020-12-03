e2f66fc9e0e530f609af4ef318c74183e2f09ef4
Steeeeeph <stephanie.vanbockhaven@hotmail.com>
2020-12-03 14:43:58 +0100

    Event handler

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 "use strict"; 
 
-// TODO Get value after Submit
+const city = document.querySelector("#city");
 
-// TODO Get Data through API (One day forecast)
+city.addEventListener('click', (event) => {
+   event.preventDefault();
 
-// TODO Overview for 5 days
+   console.log(city.value);
 
-// TODO Get data to be visible in HTML
+   // TODO Get Data through API (One day forecast)
+      // TODO Get appropriate API key and url
+      // TODO Make a promise statement 
+         // TODO fetch data from url
+         // TODO convert results in json format
+         // TODO then get the wanted data from the object list
+         // TODO if rejected: display what's wrong
+      // TODO collect data in variables
+
+   // TODO Overview for 5 days
+
+   // TODO Get data to be visible in HTML > cards container
+
+});
