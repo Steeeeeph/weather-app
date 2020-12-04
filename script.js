abbf4b8835d7104fd3130e7aaaac7ea731cef355
Steeeeeph <stephanie.vanbockhaven@hotmail.com>
2020-12-04 11:03:06 +0100

    Adding second url for multiple day forecast

--- script.js
+++ script.js
@@ -6,8 +6,12 @@ const cards = document.querySelector(".cards");
 const title = document.querySelector("span");
 let temperature;
 let weather;
+let latitude;
+let longitude;
 let date = new Date();
 let todayNumber = date.getDay();
+let date = date.getDate();
+let year = date.getFullYear();
 
 const days = [
    'Sunday',
@@ -18,6 +22,20 @@ const days = [
    'Friday',
    'Saturday'
 ];
+const months = [
+   'January',
+   'February',
+   'March',
+   'April',
+   'May',
+   'June',
+   'July',
+   'August',
+   'September',
+   'October',
+   'November',
+   'December'
+]
 let day = () => {
    days[todayNumber];
    if (days[todayNumber]) {
@@ -37,7 +55,6 @@ submit.addEventListener('click', (event) => {
       const cityValue = city.value;
       const apiKeyWeather = '12ce9e55f98edc446d7b88a0a9db3845';
       const urlWeather = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apiKeyWeather}&units=metric`;
-
       title.innerText = `in ${cityValue}`;
    
       fetch(urlWeather)
@@ -48,16 +65,18 @@ submit.addEventListener('click', (event) => {
             console.log(collect);
             temperature = collect.main.temp + '°C';
             weather = collect.weather[0].main;
-            return temperature, weather;
+            latitude = collect.coord.lat;
+            longitude = collect.coord.lon;
+            return temperature, weather, latitude, longitude;
          });
          // TODO if rejected: display what's wrong
-      
+
       // TODO display one day in HTML
       const displayCard = () => {
          let card;
          card = '<div class="card">';
-         // card += '<h1>'+ cityValue + '</h1>';
          card += '<h2>'+ day() + '</h2>';
+         card += '<h3>'+
          card += '<p>' + temperature + '</p>';
          card += '<p>' + weather + '</p>';
          card += '</div>';
@@ -65,7 +84,32 @@ submit.addEventListener('click', (event) => {
       }
       displayCard();
    // TODO Overview for 5 days
-
+      const urlWeatherDays = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=current,minutely,hourly,alerts&appid=${apiKeyWeather}&units=metric`
+      fetch(urlWeatherDays)
+         .then(response => {
+            return response.json();
+         })
+         .then(collect => {
+            console.log(collect);
+         })
+      console.log(urlWeatherDays);
    // TODO Get data to be visible in HTML > cards container
 
 });
+/*
+var d = new Date();
+var month = new Array();
+month[0] = "January";
+month[1] = "February";
+month[2] = "March";
+month[3] = "April";
+month[4] = "May";
+month[5] = "June";
+month[6] = "July";
+month[7] = "August";
+month[8] = "September";
+month[9] = "October";
+month[10] = "November";
+month[11] = "December";
+var n = month[d.getMonth()];
+*/
